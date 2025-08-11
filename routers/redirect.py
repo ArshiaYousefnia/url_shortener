a4fa26fba053d3ed67690cb283fa5378b8b93a24
ArshiaYousefnia <arshia.yousefnia@gmail.com>
2025-08-11 18:05:02 +0330

    bugfix in redirect: wrong short url get method

--- routers/redirect.py
+++ routers/redirect.py
@@ -1,14 +1,13 @@
-
 from fastapi import APIRouter
 from starlette.responses import RedirectResponse
 
 from models.session import SessionDep
-from routers.shorten import get_short_url_from_db
+from routers.shorten import get_short_url
 
 router = APIRouter()
 
 @router.get("/{short_code}")
 async def get_redirect_page(short_code: str, session: SessionDep):
-    short = get_short_url_from_db(short_code, session)
+    short = await get_short_url(short_code, session)
 
     return RedirectResponse(url=short.url)
